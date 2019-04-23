876d4d068a34f86b7e7b2a1864a94b128fa261fe
Roybal_A <Roybal.usmc@gmail.com>
2019-04-23 15:22:40 -0700

    Tables

--- main.lua
+++ main.lua
@@ -1,15 +1,25 @@
 message = 0  --global variable
-param = 500 --global variable
 
---this function doubles a value
-function IncreaseMessage (i)
-  local var = i;   --local variable
-  var = var * var
+--tables, lua starts at index 1
+testScoresA = {}
+testScoresA[1] = 95
+testScoresA[2] = 87
+testScoresA[3] = 98
 
-  return var  -- body...
-end
+--you can also do, same as index 1
+testScoresA["math"] = 88
 
-message = IncreaseMessage(param)
+--same as above
+testScoresB = {95, 87, 98}
+
+--same as above
+testScoresC = {}
+table.insert(testScoresC, 95)
+table.insert(testScoresC, 87)
+table.insert(tableScoresC, 98)
+
+--in lue you can use nil for nothing, which is like NULL
+message = testScores[1]
 
 function love.draw()
   love.graphics.setFont((love.graphics.newFont(50)))
