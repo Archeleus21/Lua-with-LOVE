6beb68b0ad771032ca04d109b806ec7d65bf3f21
Roybal_A <Roybal.usmc@gmail.com>
2019-04-23 13:43:06 -0700

    variables

--- main.lua
+++ main.lua
@@ -1,3 +1,10 @@
+message = 5
+chicken = 10
+output = chicken + 1
+
+message = message - 1
+
 function love.draw()
-    love.graphics.print("Hello World!")
+  love.graphics.setFont((love.graphics.newFont(50)))
+  love.graphics.print(message)
 end
