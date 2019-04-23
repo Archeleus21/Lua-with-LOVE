9dd033f1cfdca0d1cceaa00828a07e10d5b4008c
Roybal_A <Roybal.usmc@gmail.com>
2019-04-23 15:10:42 -0700

    comments

--- main.lua
+++ main.lua
@@ -1,13 +1,17 @@
 message = 0
 param = 500
 
+--[[
+--this function doubles a value
 function IncreaseMessage (i)
   i = i * 2
 
   return i  -- body...
 end
 
-message = IncreaseMessage(param)
+--message = IncreaseMessage(param)
+
+]]
 
 function love.draw()
   love.graphics.setFont((love.graphics.newFont(50)))
